36572c59bae09167ea5ad678cea2b653fe9b20c1
Yomumu05 <168425473+Yomumu05@users.noreply.github.com>
2024-04-29 20:06:41 +0900

    c프로그래밍 과제1

---
+++ homework_1/homework_1/homework_1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+
+	printf("문제 1번입니다.\n");
+	printf("이름 : 염현우\n");
+	printf("학과 : 컴퓨터공학과\n");
+	printf("학번 : 2451021\n");
+	printf("학년 : 1학년\n");
+
+	return 0;
+}
